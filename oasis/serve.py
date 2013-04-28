54d0f0e0a995afa487987c17c7426fdf263b064c
Kenji Yano <kenji.yano@gmail.com>
2013-04-28 20:26:12 +0900

    デフォルトのコンフィグパスを絶対パスに変更

--- oasis/serve.py
+++ oasis/serve.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 import sys
 import json
 from optparse import OptionParser
@@ -10,9 +11,11 @@ from server import ThreadingWSGIServer as WSGIServer, HTTPRequestHandler
 def main(args):
     """Start this server default on 127.0.0.1:8080"""
 
+    configfile = os.path.join(os.path.dirname(os.path.abspath(__file__)), "default.json")
+
     parser = OptionParser()
     parser.add_option('-v', '--verbose', dest='verbose', action='store_true', default=False, help='print configuration.')
-    parser.add_option('-c', '--config', dest='configfile', default='default.json', help='config file.')
+    parser.add_option('-c', '--config', dest='configfile', default=configfile, help='config file.')
     parser.add_option('-a', '--address', dest='address', default='127.0.0.1', help='server address.')
     parser.add_option('-p', '--port', dest='port', default=8080, type=int, help='server port.')
 
@@ -30,4 +33,4 @@ def main(args):
     httpd.serve_forever()
 
 if __name__ == '__main__':
-    main (sys.argv[1:])
+    main(sys.argv[1:])
