9844c6c40300e8c91b82c7e2b2cccf7c7b117a9d
Kenji Yano <kenji.yano@gmail.com>
2013-04-30 19:27:05 +0900

    pypi 対応

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 oasis
 =====
-
 a simple multifunction server (http, proxy, cgi, wsgi) written in python
+
+Requirements
+------------
+* Python 2.7 or later (not support 3.x)
+
+Features
+--------
+* nothing
+
+Setup
+-----
+::
+
+   $ easy_install oasis
+
+History
+-------
+0.1.0 (2013-4-30)
+~~~~~~~~~~~~~~~~~~
+* first release
---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+version = '0.1.0'
+name = 'oasis'
+short_description = 'a simple multifunction server (http, proxy, cgi, wsgi)'
+with open("README.md") as f:
+    long_description = f.read()
+
+classifiers = [
+   "Development Status :: 1 - Planning",
+   "License :: OSI Approved :: MIT License",
+   "Programming Language :: Python",
+   "Topic :: Software Development",
+]
+
+setup(
+    name=name,
+    version=version,
+    description=short_description,
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['oasis', 'wsgi', 'cgi', 'proxy', 'http', 'https'],
+    author='Kenji Yano',
+    author_email='kenji.yano@gmail.com',
+    url='https://github.com/yanolab/oasis',
+    license='MIT',
+    packages=['oasis', 'oasis.wsgi'],
+    package_data={'oasis': ['default.json']}
+)
